ec21419f0afadb5111ab3ae789d8aa4ed357ca02
SafidyPatrickRas <safidypatrickrasolofoniaina@gmail.com>
2026-03-01 10:56:57 +0300

    modification donnee

--- framework-test/sql/donee.sql
+++ framework-test/sql/donee.sql
@@ -14,7 +14,7 @@ INSERT INTO hotel (nom, id_aeroport) VALUES
 INSERT INTO reservation (date_arriver, nbr_passager, id_client, Id_hotel, id_aeroport, TA) VALUES
 ('2026-02-05 00:55:00', 5, '4631', 3, 2, 15),
 ('2026-02-05 01:00:00', 1,  '4394', 3, 2, 30),
-('2026-02-09 01:30:00', 2,  '8054', 1, 2, 20);
+('2026-02-09 01:30:00', 2,  '8054', 1, 1, 20);
 
 INSERT INTO token (uid) VALUES
 (UUID()),
@@ -33,9 +33,21 @@ INSERT INTO hotel_hotel_distance (from_hotel_id, to_hotel_id, km) VALUES
 
 INSERT INTO hotel_aeroport_distance (id_hotel, id_aeroport, km) VALUES
 (1, 1, 14.50),
+(1, 2, 31.20),
+(1, 3, 49.80),
+(1, 4, 28.40),
 (2, 1, 16.20),
+(2, 2, 29.90),
+(2, 3, 52.10),
+(2, 4, 30.70),
 (3, 2, 7.80),
-(4, 3, 5.40);
+(3, 1, 22.60),
+(3, 3, 44.30),
+(3, 4, 25.90),
+(4, 1, 35.10),
+(4, 2, 26.80),
+(4, 3, 5.40),
+(4, 4, 33.20);
 
 INSERT INTO consommation (description) VALUES
 ('Essence'),
